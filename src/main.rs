e59b8c482181b351e0d3fbe6362f5167d611e10e
andriim <quadr988@gmail.com>
2022-11-04 20:28:33 +0200

    init

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use html_parser::Dom;
+use html_parser::{Dom, Node};
 
 fn main() {
-    let html = r#"
+    //https://docs.rs/html_parser/latest/html_parser/enum.Node.html
+    let html_str = r#"
             <!doctype html>
             <html lang="en">
                 <head>
@@ -14,7 +15,15 @@ fn main() {
                 </body>
             </html>"#;
 
-    let dom: Result<Dom, html_parser::Error> = Dom::parse(html);
-    let str = dom.unwrap().children;
-    println!("{:?}", str)
+    let dom: Result<Dom, html_parser::Error> = Dom::parse(html_str);
+    let nodes = dom.unwrap().children;
+    for node in nodes {
+        match node {
+            Node::Text(text) => println!("text"),
+            Node::Comment(comment) => println!("comment"),
+            Node::Element(element) => {
+                println!("{:?}", element)
+            },
+        }
+    }
 }
