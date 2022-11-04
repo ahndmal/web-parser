277336597fbb3f77d7b669b563ffabf7004dcaa0
andriim <quadr988@gmail.com>
2022-11-04 19:01:09 +0200

    init

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 use html_parser::Dom;
 
 fn main() {
-    println!("Hello, world!");
+    let html = r#"
+            <!doctype html>
+            <html lang="en">
+                <head>
+                    <meta charset="utf-8">
+                    <title>Html parser</title>
+                </head>
+                <body>
+                    <h1 id="a" class="b c">Hello world</h1>
+                    </h1> <!-- comments & dangling elements are ignored -->
+                </body>
+            </html>"#;
+
+    let dom: Result<Dom, html_parser::Error> = Dom::parse(html);
+    let str = dom.unwrap().children;
+    println!("{:?}", str)
 }
