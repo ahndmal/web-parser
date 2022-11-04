c0925c674f3c5c153d1d6139bea81ee9261f79f0
andriim <quadr988@gmail.com>
2022-11-04 21:05:10 +0200

    init

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use html_parser::{Dom, Node};
+use html_parser::{Dom, Node, Element};
+use scraper::{Html, Selector};
+use html5ever::driver::ParseOpts;
+use html5ever::tendril::TendrilSink;
+use html5ever::tree_builder::TreeBuilderOpts;
+use html5ever::{parse_document, serialize};
 
 fn main() {
-    //https://docs.rs/html_parser/latest/html_parser/enum.Node.html
+
     let html_str = r#"
             <!doctype html>
             <html lang="en">
@@ -11,18 +16,35 @@ fn main() {
                 </head>
                 <body>
                     <h1 id="a" class="b c">Hello world</h1>
-                    </h1> <!-- comments & dangling elements are ignored -->
+                    <div id="div1">one</div>
+                    <div>two</div>
                 </body>
             </html>"#;
 
+    let doc = Html::parse_document(html_str);
+    let divs_delector = Selector::parse("div").unwrap();
+    for elem in doc.select(&divs_delector) {
+        let elem_value = elem.value();
+        if elem_value.attr("id") != None {
+            println!("{:?}", elem_value.attr("id"));
+        }
+    }
+}
+
+fn core_html(html_str: &str) {
+    //https://docs.rs/html_parser/latest/html_parser/enum.Node.html
     let dom: Result<Dom, html_parser::Error> = Dom::parse(html_str);
     let nodes = dom.unwrap().children;
     for node in nodes {
         match node {
             Node::Text(text) => println!("text"),
-            Node::Comment(comment) => println!("comment"),
+            Node::Comment(comment) => println!("comment is {comment}"),
             Node::Element(element) => {
-                println!("{:?}", element)
+                let body: &Node = &element.children[1];
+                if let Node::Element(element) = body {
+                    let div2 = &element.children[2];
+                    println!("{:?}", div2)
+                }
             },
         }
     }
